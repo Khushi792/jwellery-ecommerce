fb00360a1afc495ee5529ba4dd13c3de702c7918
KHUSHI AGRAWAL <118662644+Khushi792@users.noreply.github.com>
2025-12-17 15:34:33 +0530

    added files

---
+++ home.js
@@ -0,0 +1,14 @@
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+    topBtn.style.display = window.scrollY > 400 ? "block" : "none";
+});
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
